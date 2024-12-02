d9d47beb01a3438cd6c6707dbe9655acb11028f2
weihong.xu <xuweihong.cn@gmail.com>
2024-12-02 15:24:09 +0800

    fix typo

--- db-backup.sh
+++ db-backup.sh
@@ -10,6 +10,6 @@ docker compose run -T -u 999 mysql_backup bash <<EOF
 set -e 
 
 cd /tmp/backup/data
-xtrabackup --backup --host=ferry_mysql --target-dir=/tmp/backup/data --data-dir=/var/lib/mysql --user=root --password=$DB_PASSWORD
+xtrabackup --backup --host=ferry_mysql --target-dir=/tmp/backup/data --datadir=/var/lib/mysql --user=root --password=$DB_PASSWORD
 
 EOF
