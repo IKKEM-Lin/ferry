1f935a0e820d8bb98d8d797a486100e65175fd41
weihong.xu <xuweihong.cn@gmail.com>
2024-12-02 15:27:06 +0800

    create back folder

--- db-backup.sh
+++ db-backup.sh
@@ -9,6 +9,7 @@ fi
 docker compose run -T -u 999 mysql_backup bash <<EOF
 set -e 
 
+mkdir -p /tmp/backup/data
 cd /tmp/backup/data
 xtrabackup --backup --host=ferry_mysql --target-dir=/tmp/backup/data --datadir=/var/lib/mysql --user=root --password=$DB_PASSWORD
 
