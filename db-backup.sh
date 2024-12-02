74dcf470f62e781fe8abe4c9cbf7496e67c08b8f
weihong.xu <xuweihong.cn@gmail.com>
2024-12-02 15:21:16 +0800

    db backup: read password if not defined

--- db-backup.sh
+++ db-backup.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 set -e 
 
+if [ -z "$DB_PASSWORD" ]; then
+    read -s -p "Please enter the db password: " DB_PASSWORD
+    echo  # Move to a new line after reading the password
+fi
+
 docker compose run -T -u 999 mysql_backup bash <<EOF
 set -e 
 
