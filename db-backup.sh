80ea7493aa3735a746fd0dc7a0b8deb38c60dd93
weihong.xu <xuweihong.cn@gmail.com>
2024-12-02 15:45:55 +0800

    tar backup data

--- db-backup.sh
+++ db-backup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd $(dirname "$0")
-set -e 
+set -e
 
 if [ -z "$DB_PASSWORD" ]; then
     read -s -p "Please enter the db password: " DB_PASSWORD
@@ -12,16 +12,20 @@ TODAY=$(date +%Y-%m-%d)
 BACKUP_DIR=/tmp/backup/$TODAY
 
 docker compose run -T -u 999 mysql_backup bash <<EOF
-set -e 
+set -e
 
 if [ -d "$BACKUP_DIR" ]; then
     echo "The directory $BACKUP_DIR already exists. Exiting."
-    exit 0 
+    exit 0
 fi
-
 
 mkdir -p $BACKUP_DIR
 cd $BACKUP_DIR
 xtrabackup --backup --host=ferry_mysql --target-dir=$BACKUP_DIR --datadir=/var/lib/mysql --user=root --password=$DB_PASSWORD
 
+tar -cf ../$TODAY.tar .
+chmod 666 ../$TODAY.tar
+( cd .. && rm -rf $BACKUP_DIR )
 EOF
+
+( cd ./backup && xz -z $TODAY.tar )
