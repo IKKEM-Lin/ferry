27bf4311a47ae2116219ea0e70ce8111dd61dd2f
weihong.xu <xuweihong.cn@gmail.com>
2024-12-02 16:01:00 +0800

    refactor backup script

--- db-backup.sh
+++ db-backup.sh
@@ -2,12 +2,6 @@
 cd $(dirname "$0")
 set -e
 
-if [ -z "$DB_PASSWORD" ]; then
-    read -s -p "Please enter the db password: " DB_PASSWORD
-    echo  # Move to a new line after reading the password
-fi
-
-
 TODAY=$(date +%Y-%m-%d)
 
 if [ -f ./backup/$TODAY.tar.xz ]; then
@@ -15,6 +9,10 @@ if [ -f ./backup/$TODAY.tar.xz ]; then
     exit 0
 fi
 
+if [ -z "$DB_PASSWORD" ]; then
+    read -s -p "Please enter the db password: " DB_PASSWORD
+    echo  # Move to a new line after reading the password
+fi
 
 docker compose run -T -u 999 mysql_backup bash <<EOF
 set -e
