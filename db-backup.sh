0f852bf5ecb90ead72fbbc499cdfa1711d93abf9
weihong.xu <xuweihong.cn@gmail.com>
2024-12-02 15:35:22 +0800

    implement daily backup script

--- db-backup.sh
+++ db-backup.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+cd $(dirname "$0")
 set -e 
 
 if [ -z "$DB_PASSWORD" ]; then
@@ -6,11 +7,21 @@ if [ -z "$DB_PASSWORD" ]; then
     echo  # Move to a new line after reading the password
 fi
 
+
+TODAY=$(date +%Y-%m-%d)
+BACKUP_DIR=/tmp/backup/$TODAY
+
 docker compose run -T -u 999 mysql_backup bash <<EOF
 set -e 
 
-mkdir -p /tmp/backup/data
-cd /tmp/backup/data
-xtrabackup --backup --host=ferry_mysql --target-dir=/tmp/backup/data --datadir=/var/lib/mysql --user=root --password=$DB_PASSWORD
+if [ -d "$BACKUP_DIR" ]; then
+    echo "The directory $BACKUP_DIR already exists. Exiting."
+    exit 0 
+fi
+
+
+mkdir -p $BACKUP_DIR
+cd $BACKUP_DIR
+xtrabackup --backup --host=ferry_mysql --target-dir=$BACKUP_DIR --datadir=/var/lib/mysql --user=root --password=$DB_PASSWORD
 
 EOF
